dd65267c67d3bd2a168329ebfa8b9b5dd01e5193
KSM <wktndudtls@gmail.com>
2019-08-21 21:52:47 +0900

    MemberVo

--- Pigeon_Market/src/com/pigeonMarket/member/model/vo/Member.java
+++ Pigeon_Market/src/com/pigeonMarket/member/model/vo/Member.java
@@ -1,5 +1,136 @@
 package com.pigeonMarket.member.model.vo;
 
+import java.sql.Date;
+
 public class Member {
+	
+	private String userId;
+	private String userPwd;
+	private String userName;
+	private Date birthDate;
+	private String gender;
+	private String email;
+	private String phone;
+	private String address;
+	private Date joinDate;
+	private String withdrawal;
+	private String blackCode;
+	
+	public Member() {
+		
+	}
+
+	public Member(String userId, String userPwd, String userName, Date birthDate, String gender, String email,
+			String phone, String address, Date joinDate, String withdrawal, String blackCode) {
+		super();
+		this.userId = userId;
+		this.userPwd = userPwd;
+		this.userName = userName;
+		this.birthDate = birthDate;
+		this.gender = gender;
+		this.email = email;
+		this.phone = phone;
+		this.address = address;
+		this.joinDate = joinDate;
+		this.withdrawal = withdrawal;
+		this.blackCode = blackCode;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserPwd() {
+		return userPwd;
+	}
+
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Date getJoinDate() {
+		return joinDate;
+	}
+
+	public void setJoinDate(Date joinDate) {
+		this.joinDate = joinDate;
+	}
+
+	public String getWithdrawal() {
+		return withdrawal;
+	}
+
+	public void setWithdrawal(String withdrawal) {
+		this.withdrawal = withdrawal;
+	}
+
+	public String getBlackCode() {
+		return blackCode;
+	}
+
+	public void setBlackCode(String blackCode) {
+		this.blackCode = blackCode;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [userId=" + userId + ", userPwd=" + userPwd + ", userName=" + userName + ", birthDate="
+				+ birthDate + ", gender=" + gender + ", email=" + email + ", phone=" + phone + ", address=" + address
+				+ ", joinDate=" + joinDate + ", withdrawal=" + withdrawal + ", blackCode=" + blackCode + "]";
+	}
+	
+	
 
 }
