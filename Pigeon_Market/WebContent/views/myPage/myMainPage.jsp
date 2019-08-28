0dc64eef3eef8c893d3971338ac315339a261a3f
academy_ksm <wktndudtls@gmail.com>
2019-08-29 08:45:33 +0900

    ksm
    0829_0845

--- Pigeon_Market/WebContent/views/myPage/myMainPage.jsp
+++ Pigeon_Market/WebContent/views/myPage/myMainPage.jsp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <%@ page language="java" contentType="text/html; charset=EUC-KR"
     pageEncoding="EUC-KR"%>
 <!DOCTYPE html>
@@ -22,4 +23,30 @@
 	
 	</script>
 </body>
+=======
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>³» ¸ÞÀÎÆäÀÌÁö</title>
+</head>
+<body>
+	<%@ include file="../common/menubar.jsp" %>
+
+	<div class="mainform">
+
+		<div class="btndiv" id="" onclick="location.href='<%= contextPath %>/buyList.deal'">±¸¸ÅÁ¤º¸</div>
+		<div class="btndiv" id="" onclick="location.href='<%= contextPath %>/sellInfoForm.deal">ÆÇ¸ÅÁ¤º¸</div>
+		<!--  <div class="btndiv" id="¹è¼ÛÁ¤º¸" onclick="delivinfo();">¹è¼ÛÁ¤º¸</div>  -->
+		<div class="btndiv" id="" onclick="location.href='<%= contextPath %>/checkMyPwdForm.me'">È¸¿øÁ¤º¸ ¼öÁ¤</div>
+
+	</div>
+	<script>
+
+	
+	</script>
+</body>
+>>>>>>> parent of d05a9a5... ìˆ˜ì •ë³¸
 </html>
--- Pigeon_Market/src/com/pigeonMarket/dealInfo/model/vo/Deal.java
+++ Pigeon_Market/src/com/pigeonMarket/dealInfo/model/vo/Deal.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.pigeonMarket.dealInfo.model.vo;
 
 import java.sql.Date;
@@ -144,3 +145,130 @@ public class Deal {
 	
 	
 }
+=======
+package com.pigeonMarket.dealInfo.model.vo;
+
+import java.sql.Date;
+
+public class Deal {
+	
+	private int productNo;				// »óÇ°¹øÈ£
+	private int price;					// °¡°Ý
+	private String pId;					// ÆÇ¸ÅÀÚ ¾ÆÀÌµð
+	private String bId;					// ±¸¸ÅÀÚ ¾ÆÀÌµð
+	private String categoryName;		// Ä«Å×ÄÚ¸® ÀÌ¸§
+	private String title;				// °Ô½Ã¹É Á¦¸ñ
+	private Date regDate; 				// µî·Ï³¯Â¥
+	private Date approvalDate;			// ½ÂÀÎ³¯Â¥
+	private Date dealDate;				// ÆÇ¸Å³¯Â¥
+	private String state;				// ÆÇ¸Å¿Ï·á »óÅÂ
+	
+	public Deal() {
+		
+	}
+
+	public Deal(int productNo, int price, String pId, String bId, String categoryName, String title, Date regDate,
+			Date approvalDate, Date dealDate, String state) {
+		super();
+		this.productNo = productNo;
+		this.price = price;
+		this.pId = pId;
+		this.bId = bId;
+		this.categoryName = categoryName;
+		this.title = title;
+		this.regDate = regDate;
+		this.approvalDate = approvalDate;
+		this.dealDate = dealDate;
+		this.state = state;
+	}
+
+	public int getProductNo() {
+		return productNo;
+	}
+
+	public void setProductNo(int productNo) {
+		this.productNo = productNo;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getpId() {
+		return pId;
+	}
+
+	public void setpId(String pId) {
+		this.pId = pId;
+	}
+
+	public String getbId() {
+		return bId;
+	}
+
+	public void setbId(String bId) {
+		this.bId = bId;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+
+	public Date getApprovalDate() {
+		return approvalDate;
+	}
+
+	public void setApprovalDate(Date approvalDate) {
+		this.approvalDate = approvalDate;
+	}
+
+	public Date getDealDate() {
+		return dealDate;
+	}
+
+	public void setDealDate(Date dealDate) {
+		this.dealDate = dealDate;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	@Override
+	public String toString() {
+		return "Deal [productNo=" + productNo + ", price=" + price + ", pId=" + pId + ", bId=" + bId + ", categoryName="
+				+ categoryName + ", title=" + title + ", regDate=" + regDate + ", approvalDate=" + approvalDate
+				+ ", dealDate=" + dealDate + ", state=" + state + "]";
+	}
+
+	
+}
+>>>>>>> parent of d05a9a5... ìˆ˜ì •ë³¸
